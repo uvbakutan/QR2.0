8d53b3570f7c4b4c7c07b6fcb55d8d02cbf2ca7f
Halil <uvbakutan@users.noreply.github.com>
2015-03-04 04:32:14 +0900

    implemented simple communication protocol between arduino and processing

--- arudino/QR2_0/QR2_0.ino
+++ arudino/QR2_0/QR2_0.ino
@@ -4,7 +4,7 @@
 #include <I2Cdev.h>
 #include <EEPROM.h>
 
-//#define DEBUG
+#define DEBUG
 #include "DebugUtils.h"
 #include "CommunicationUtils.h"
 #include "FreeIMU.h"
@@ -13,6 +13,8 @@
 
 char cmd;
 float ypr[3];
+int value(0);
+int type(0);
 
 int i=0;
 int bat = 0;
@@ -30,26 +32,16 @@ void setup() {
 
 
 void loop() {
-  my3IMU.getYawPitchRoll(ypr);
+  //my3IMU.getYawPitchRoll(ypr);
+  my3IMU.getEuler(ypr);
   
-  if(Serial1.available()>0)
+  if(Serial1.available()>3)
     readSerial();
-  
-   
-  if(i++%10==0){
-    Serial1.print("eul: ");
-    Serial1.print(ypr[0]);
-    Serial1.print("  ");
-    Serial1.print(ypr[1]);
-    Serial1.print("  ");
-    Serial1.println(ypr[2]);
-     Serial1.print("  ");
-     
-     bat=analogRead(3);
-    Serial1.println(float(bat*0.009765625));
-  }
-  
+ 
+    if(i++%10==0)
+     writeSerial();
 }
+
 
 void initializeMotors(){
   pinMode(10,OUTPUT);
@@ -65,46 +57,45 @@ void initializeMotors(){
 }
 
 void readSerial(){
-    char input;
-    input = Serial1.read();
-    Serial1.print("recieved ");
-    Serial1.println(input);
-    
-    switch (input){
-      case 'z' : {
-          analogWrite(5,0);
-          analogWrite(6,0);
-          analogWrite(9,0);
-          analogWrite(10,0);
-      } break ;
-      
-      case 'f' : {
-          analogWrite(5,255);
-          analogWrite(6,255);
-          analogWrite(9,255);
-          analogWrite(10,255);
-      } break ;
-      
-      case 'h' : {
-          analogWrite(5,150);
-          analogWrite(6,150);
-          analogWrite(9,150);
-          analogWrite(10,150);
-      } break ;
-      
-      case 's' : {
-          analogWrite(5,30);
-          analogWrite(6,30);
-          analogWrite(9,30);
-          analogWrite(10,30);
-      } break ;
-      
-      default : {
-          analogWrite(5,0);
-          analogWrite(6,0);
-          analogWrite(9,0);
-          analogWrite(10,0);   
-      }break;
+    int startT(0);
+    int endT(0);
+
+    startT=Serial1.read();
+    if( startT!=0xc5){
+      Serial1.flush();
+      Serial1.println("flushing");
+      return;
     }
+    
+    type = Serial1.read();
+    value = Serial1.read();
+    endT = Serial1.read();
+    
+    if(endT!=0x5c){
+      Serial1.println("error");
+      return;
+    }
+    
+    analogWrite(5,value);
+    analogWrite(6,value);
+    analogWrite(9,value);
+    analogWrite(10,value);   
+}
+
+void writeSerial(){
+  Serial1.print("msg,");
+  Serial1.print(ypr[0]);
+  Serial1.print(",");
+  Serial1.print(ypr[1]);
+  Serial1.print(",");
+  Serial1.print(ypr[2]);
+   
+  bat=analogRead(3);
+  Serial1.print(",");
+  Serial1.print(float(bat*0.009765625));
+  Serial1.print(",");
+  Serial1.print(value);
+  Serial1.print(",");
+  Serial1.println(type);
 }
 
